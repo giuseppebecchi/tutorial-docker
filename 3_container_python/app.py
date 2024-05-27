6fbf6aaaa5932d455f3f53c47f5b7d51e113d565
Marco Bertini <marco.bertini@unifi.it>
2024-05-27 15:51:32 +0200

    FIX: indentation

--- 3_container_python/app.py
+++ 3_container_python/app.py
@@ -6,13 +6,12 @@ import socket
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-
     html = "<h3>Hello {name}!</h3>" \
            "<b>Password:</b> {my_password}"
     return html.format(
         name=os.getenv("NAME", "world"),
         my_password=os.getenv("PASSWORD", "Doh, you didn't set the password in the environment variable!")
     )
-
